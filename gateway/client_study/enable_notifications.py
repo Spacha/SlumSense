8690f3d6a5b154b8bb004818adb95820748e2485
Spacha <miikasikala96@gmail.com>
2021-12-03 05:52:14 +0200

    API key from environment

--- gateway/client_study/enable_notifications.py
+++ gateway/client_study/enable_notifications.py
@@ -6,12 +6,18 @@ Example showing how to add notifications to a characteristic and handle the resp
 Updated on 2019-07-03 by hbldh <henrik.blidh@gmail.com>
 """
 
+import os
 import sys
 import asyncio
 import platform
+import requests
+from hashlib import md5
 
 from bleak import BleakClient
 
+# SlumCloud API
+API_URL     = "https://slum.spacha.dev/api/v1"
+GATEWAY_KEY = "aQXcflqN"
 
 # you can change these to match your device or override them from the command line
 CHARACTERISTIC_UUID = "f3641401-00b0-4240-ba50-05ca45bf8abc"
@@ -21,8 +27,32 @@ ADDRESS = (
     #else "B9EA5233-37EF-4DD6-87A8-2A875E821C46"
 )
 
+def get_hashed_api_key():
+    ''' Get APi ke '''
+    fail = False
+    api_key = ""
+    try:
+        api_key = os.environ["API_KEY"]
+    except Exception:
+        fail = True
+
+    if fail or len(api_key) > 3:
+        return md5(api_key.encode()).hexdigest()
+    else:
+        raise Exception("Missing API key! Set it as an environment variable 'API_KEY'.")
+
 def send_data(temp, pres, humi):
-    pass
+    response = requests.post("{API_URL}/measurements", data = {
+        "API_KEY":      get_hashed_api_key(),
+        "gateway_key":  GATEWAY_KEY,
+        "temperature":  temp,
+        "pressure":     temp,
+        "humidity":     humi
+    })
+
+    print(response)
+    print("Status: ", + str(response.status_code))
+    print("Message: " + response.text)
 
 
 def notification_handler(sender, data):
@@ -61,3 +91,8 @@ async def main(address, char_uuid):
 
 if __name__ == "__main__":
     asyncio.run(
+        main(
+            sys.argv[1] if len(sys.argv) > 1 else ADDRESS,
+            sys.argv[2] if len(sys.argv) > 2 else CHARACTERISTIC_UUID,
+        )
+    )
