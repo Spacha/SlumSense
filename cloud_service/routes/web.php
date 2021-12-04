5bbac1d898e1dd365dcc9f64884bea4bed3d4821
Spacha <miikasikala96@gmail.com>
2021-12-04 09:19:01 +0200

    Enable/disable registration from env

--- cloud_service/routes/web.php
+++ cloud_service/routes/web.php
@@ -17,7 +17,7 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Auth::routes(['register' => false]);
+Auth::routes(['register' => env('APP_ALLOW_NEW_USERS')]);
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 Route::get('/measurements/{measurement}', [App\Http\Controllers\MeasurementController::class, 'show']);
